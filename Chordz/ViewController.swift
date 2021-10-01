7a92016c7f50881434b645b525026983abb4e2f9
mxm0312 <maxim_perehod@mail.ru>
2021-10-01 14:11:01 +0300

    Initial Commit

---
+++ Chordz/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Chordz
+//
+//  Created by Maxim Perehod on 01.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
